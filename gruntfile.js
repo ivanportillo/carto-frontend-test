d526611cf88385e1c1f0f0279fbd40f4355060e1
Iván Portillo Leal <ivanportillo@me.com>
2017-10-23 17:44:29 +0200

    Add dependencies and improve .gitignore and package.json

--- gruntfile.js
+++ gruntfile.js
@@ -4,9 +4,25 @@ module.exports = grunt => {
     grunt.initConfig({
         browserify: {
             dist: {
-                src: ['src/**/*.js'],
+                options: {
+                    transform: [['babelify', {presets: ['es2015']}]]
+                 },
+                src: ['src/**/*.js', '!src/resources/*.js'],
                 dest: 'public/app.js'
             }
-        }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            combine: {
+                files: {
+                  'public/styles/main.css': ['!src/styles/**/*.min.css', 'src/styles/**/*.css']
+                }
+            }
+        }          
     });
+
+    grunt.registerTask('default', ['browserify', 'cssmin']);
 };
